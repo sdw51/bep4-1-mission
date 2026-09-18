9e92a72424abc2fe258d1c04ad0fad84be7f9bee
sdw98 <sdw9851@gmail.com>
2026-09-18 12:51:34 +0900

    0019

--- src/main/java/com/back/boundedContext/post/domain/PostMember.java
+++ src/main/java/com/back/boundedContext/post/domain/PostMember.java
@@ -6,12 +6,15 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Entity
 @NoArgsConstructor
 @Table(name = "POST_MEMBER")
 public class PostMember extends ReplicaMember {
-    public PostMember(String username, String password, String nickname) {
-        super(username, password, nickname);
+    public PostMember(int id, LocalDateTime createDate, LocalDateTime modifyDate,
+                      String username, String password, String nickname) {
+        super(id, createDate, modifyDate, username, password, nickname);
     }
 }
