a98ed21a7da5ed770b88c19ee0c6848f863b09ea
sdw98 <sdw9851@gmail.com>
2026-09-18 16:49:19 +0900

    0024

---
+++ src/main/java/com/back/boundedContext/cash/out/WalletRepository.java
@@ -0,0 +1,8 @@
+package com.back.boundedContext.cash.out;
+
+import com.back.boundedContext.cash.domain.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WalletRepository extends JpaRepository<Wallet, Integer> {
+
+}
