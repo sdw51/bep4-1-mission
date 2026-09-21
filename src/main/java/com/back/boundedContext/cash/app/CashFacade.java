ec648dae94c738ee3771162ccb51d005697742db
sdw98 <sdw9851@gmail.com>
2026-09-21 10:30:21 +0900

    0027

--- src/main/java/com/back/boundedContext/cash/app/CashFacade.java
+++ src/main/java/com/back/boundedContext/cash/app/CashFacade.java
@@ -1,8 +1,8 @@
 package com.back.boundedContext.cash.app;
 
-
 import com.back.boundedContext.cash.domain.CashMember;
 import com.back.boundedContext.cash.domain.Wallet;
+import com.back.shared.cash.dto.CashMemberDto;
 import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,9 +13,9 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class CashFacade {
+    private final CashSupport cashSupport;
     private final CashSyncMemberUseCase cashSyncMemberUseCase;
     private final CashCreateWalletUseCase cashCreateWalletUseCase;
-    private final CashSupport cashSupport;
 
     @Transactional
     public CashMember syncMember(MemberDto member) {
@@ -23,8 +23,7 @@ public class CashFacade {
     }
 
     @Transactional
-    public Wallet createWallet(CashMember holder) {
-
+    public Wallet createWallet(CashMemberDto holder) {
         return cashCreateWalletUseCase.createWallet(holder);
     }
 
