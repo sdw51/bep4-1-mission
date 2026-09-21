f0e027b9d8875bfdc2e1e5dcd0d7df06ab8684de
sdw98 <sdw9851@gmail.com>
2026-09-21 17:29:17 +0900

    0035

--- src/main/java/com/back/boundedContext/market/in/MarketEventListener.java
+++ src/main/java/com/back/boundedContext/market/in/MarketEventListener.java
@@ -1,6 +1,8 @@
 package com.back.boundedContext.market.in;
 
 import com.back.boundedContext.market.app.MarketFacade;
+import com.back.shared.cash.event.CashOrderPaymentFailedEvent;
+import com.back.shared.cash.event.CashOrderPaymentSucceededEvent;
 import com.back.shared.market.event.MarketMemberCreatedEvent;
 import com.back.shared.member.event.MemberJoinedEvent;
 import com.back.shared.member.event.MemberModifiedEvent;
@@ -35,4 +37,15 @@ public class MarketEventListener {
         marketFacade.createCart(event.getMember());
     }
 
+    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @Transactional(propagation = REQUIRES_NEW)
+    public void handle(CashOrderPaymentSucceededEvent event) {
+        marketFacade.handle(event);
+    }
+
+    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @Transactional(propagation = REQUIRES_NEW)
+    public void handle(CashOrderPaymentFailedEvent event) {
+        marketFacade.handle(event);
+    }
 }
