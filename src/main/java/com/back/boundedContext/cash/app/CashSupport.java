e0a58f3f1456ddd750485175a9673ed235da00f5
sdw98 <sdw9851@gmail.com>
2026-09-21 09:40:36 +0900

    0026

---
+++ src/main/java/com/back/boundedContext/cash/app/CashSupport.java
@@ -0,0 +1,30 @@
+package com.back.boundedContext.cash.app;
+
+import com.back.boundedContext.cash.domain.CashMember;
+import com.back.boundedContext.cash.domain.Wallet;
+import com.back.boundedContext.cash.out.CashMemberRepository;
+import com.back.boundedContext.cash.out.WalletRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CashSupport {
+    private final CashMemberRepository cashMemberRepository;
+    private final WalletRepository walletRepository;
+
+    @Transactional(readOnly = true)
+    public Optional<CashMember> findMemberByUsername(String username) {
+        return cashMemberRepository.findByUsername(username);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Wallet> findWalletByHolder(CashMember holder) {
+        return walletRepository.findByHolder(holder);
+    }
+}
+
+
